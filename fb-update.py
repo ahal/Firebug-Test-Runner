21b5dbda060cca83c1cdae5a8ee3b6f42a2bdc80
Andrew <halbersa@gmail.com>
2010-05-12 13:26:41 -0700

    Update fb-update.py

--- fb-update.py
+++ fb-update.py
@@ -1,83 +1,13 @@
 # coding=UTF-8
-## This script will grab all of the latest firebug releases (one for each version), as well as 
-## the fbtests and testlists and store them in the current directory
+## This script will grab all of the latest firebug releases, as well as 
+## the fbtests and testlists and store them on the specified webserver
 from ConfigParser import ConfigParser
-import urllib, sgmllib, sys, os, pysvn, zipfile, glob, getopt
-
-class LinkParser(sgmllib.SGMLParser):
-    "Class for parsing HTML hyperlinks"
-    def __init__(self, verbose=0):
-        "Initialize parse object"
-        sgmllib.SGMLParser.__init__(self, verbose)
-        self.links = []
-    def parse(self, s):
-        "Parse string s"
-        self.feed(s)
-        self.close()
-    def start_a(self, attributes):
-        "Process a hyperlink and its attributes"
-        for name, value in attributes:
-            if name == "href":
-                self.links.append(value)
-    def get_links(self):
-        "Return the hyperlinks"
-        return self.links
-
-# Helper to return a list of links from a website
-def get_links(url):
-    # HTTP Request
-    f = urllib.urlopen(url)
-    s = f.read()
-    f.close()
-    # Parse the response
-    parser = LinkParser()
-    parser.parse(s)
-    return parser.get_links()
-    
-# Return 1 is b is greater than a, 0 otherwise
-def compare_firebug(a, b):
-    if a == "" and b != "": return 1
-    elif a != "" and b == "": return 0
-    a, b = a.split("."), b.split(".")
-    try:
-        if int(a[0]) < int(b[0]) or int(a[1].replace("X", "")) < int(b[1].replace("X", "")) or int(a[2][0]) < int(b[2][0]) or a[2][1] < b[2][1] or int(a[2][2:]) < int(b[2][2:]):
-            return 1
-    except IndexError:
-        if len(a[2]) < len(b[2]):
-            return 1
-    return 0
-
-def extract_xpi(url):
-    links = get_links(url)
-    # Find maximum version number
-    max = ""
-    for link in links:
-        try:
-            if link[0:7] == "firebug" and compare_firebug(max[8:-4], link[8:-4]) == 1:
-                max = link
-        except IndexError:
-            pass
-    # Grab the most recent .xpi file
-    os.system("wget -N " + url + max)
-
-def make_archive(dir, zipper, base=""):
-    """
-        Zips the contents of the directory specified by 'dir' into the zipper
-        specified by 'zipper'.  Base is used for recursion and should not be used when calling the method
-    """
-    try:
-        for file in glob.glob(dir + "/*"):
-            if os.path.isdir(file):
-                make_archive(file, zipper, base + os.path.basename(file) + "/")
-            else:
-                zipper.write(file, base + os.path.basename(file), zipfile.ZIP_DEFLATED)
-        return 1
-    except: return 0
-
+import os, sys, getopt
 
 # Initialization
 config = ConfigParser()
-config.read(os.getcwd() + "/fbtest.conf")
+config.read(os.getcwd() + "/fb-test-runner.config")
+# Parse command lines
 try:
     opts, args = getopt.getopt(sys.argv[1:], "s:")
 except getopt.GetoptError, err:
@@ -91,36 +21,17 @@ for o, a in opts:
     else:
         assert False, "unhandled option"
 
-
-# Get list of currently available versions and download them
-##links = get_links("http://getfirebug.com/releases/firebug/")
-##for link in links:
-##    try:
-##        if int(link[0]):
-##            extract_xpi("http://getfirebug.com/releases/firebug/" + link)
-##    except ValueError:
-##        pass
-##        
-##        
-client = pysvn.Client()
-### Get the current test extensions from svn
-##if not os.path.isdir(os.path.curdir + "/fbtest/.svn"):
-##    client.checkout("http://fbug.googlecode.com/svn/fbtest/", os.getcwd() + "/fbtest")
-##else:
-##    client.update(os.path.curdir + "/fbtest")
-##for dir in os.listdir(os.path.curdir + "/fbtest/branches"):
-##    if os.path.isdir(os.path.curdir + "/fbtest/branches/" + dir) and dir[:6] == "fbtest":
-##        zout = zipfile.ZipFile(dir + ".xpi", "w")
-##        make_archive(os.path.curdir + "/fbtest/branches/" + dir, zout)
-##        zout.close()
-
-# Get the tests and testlists from directory
-if not os.path.isdir(os.path.curdir + "/getfirebug.com/tests/.svn"):
-    client.checkout("http://fbug.googlecode.com/svn/tests/", os.getcwd() + "/getfirebug.com/tests")
-else:
-    client.update(os.path.curdir + "/getfirebug.com/tests")
-os.system("wget -N http://getfirebug.com/tests/issues/issue1205/中文 以及空格.html")
-os.system("cp -r -u " + os.getcwd() + "/getfirebug.com/tests " + serverpath)
-
-
-
+# Grab the test_bot.config file
+os.system("wget -N http://getfirebug.com/releases/firebug/test-bot.config")
+test_bot = ConfigParser()
+test_bot.read(os.getcwd() + "/test-bot.config")
+# For each section in the config file, download the specified files and move them to the webserver
+for section in test_bot.sections():
+    os.system("wget -N --directory-prefix=./" + section.lower() + " " + test_bot.get(section, "FIREBUG_XPI"))
+    os.system("wget -N --directory-prefix=./" + section.lower() + " " + test_bot.get(section, "FBTEST_XPI"))
+    if not os.path.isdir(os.path.curdir + section + "/.svn"):
+        os.system("svn co " + " http://fbug.googlecode.com/svn/tests/" + " " + os.getcwd() + "/" + section.lower() + "/tests -r " + test_bot.get(section, "SVN_REVISION"))
+    else:
+        os.system(section.lower() + "/svn update -r " + test_bot.get(section, "SVN_REVISION"))
+    os.system("cp -r ./" + section.lower() + " " + serverpath)
+    
