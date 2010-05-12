73aae769c119942900b6131b9edd77e9341d4797
Andrew <halbersa@gmail.com>
2010-05-12 15:09:26 -0700

    Changes to fb-run.py

--- fb-run.py
+++ fb-run.py
@@ -11,62 +11,45 @@ def kill_firefox():
             os.kill(int(pid), signal.SIGHUP)
             break
 
-def ping_firefox():
-    for line in os.popen("ps xa"):
-        fields = line.split()
-        process = fields[4]
-        if process.find("firefox") > 0:
-            return 1
-    return 0    
-
 # Initialization
 config = ConfigParser()
-config.read(os.getcwd() + "/fbtest.conf")
-
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "s:v:p:")
-except getopt.GetoptError, err:
-    print str(err) # will print something like "option -a not recognized"
-    sys.exit(2)
+config.read("./fb-test-runner.config")
 # Retrieve variables from command line or config file
 serverpath = config.get("run", "serverpath")
 profile = config.get("run", "profile")
 firebug_version = config.get("run", "firebug_version")
-fbtest_version = config.get("run", "fbtest_version")
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "s:v:p:")
+except getopt.GetoptError, err:
+    print str(err) 
+    sys.exit(2)
 for o, a in opts:
     if o == "-s":
         serverpath = a
     elif o == "-v":
         firebug_version = a
-        fbtest_version = a
     elif 0 == "-p":
         profile = a 
     else:
         assert False, "unhandled option"
 
-# Find appropriate .xpi files based on the version, if they exist
-firebug, fbtest = "", ""
-for file in os.listdir(os.getcwd()):
-    if file[:9 + len(firebug_version)] == "firebug-" + firebug_version + "X":
-        firebug = file
-    elif file[:8 + len(firebug_version)] == "firebug-" + firebug_version and firebug == "" :
-        firebug = file
-    elif file[:6 + len(fbtest_version)] == "fbtest" + fbtest_version and fbtest == "":
-        fbtest = file
-# Make sure version actually exists
-if firebug == "":
-    print "Couldn't find Firebug extension with version: " + firebug_version
-    sys.exit(2)
-elif fbtest == "":
-    print "Couldn't find FBTest extension with version: " + fbtest_version
-    sys.exit(2)
-    
+
+
+serverpath = serverpath + "firebug" + firebug_version
+
+# Grab the extensions
+os.system("wget " + serverpath + "/firebug.xpi " + serverpath + "/fbtest.xpi")
+
 # If firefox is running, kill it (needed for mozrunner)
 kill_firefox()
 
-sleep(5)
+sleep(2)
 
-print profile
-p = subprocess.Popen(shlex.split("mozrunner -n -p " + profile + " --addons=\"" + os.getcwd() + "/" + firebug + "\",\"" + os.getcwd() + "/" + fbtest + "\""))
-p = subprocess.Popen(shlex.split("firefox -runFBTests " + serverpath + "tests/content/testlists/firebug" + fbtest_version + ".html"))
-    
+subprocess.Popen(shlex.split("mozrunner -n -p " + profile + " --addons=\"./firebug.xpi\",\"./fbtest.xpi\""))
+os.system("firefox -runFBTests " + serverpath + "/tests/content/testlists/firebug" + firebug_version + ".html")# -P " + profile)
+
+# Cleanup
+os.system("rm ./firebug.xpi")
+os.system("rm ./fbtest.xpi")
+kill_firefox()
+sys.exit(0)
--- fb-update.py
+++ fb-update.py
@@ -6,15 +6,15 @@ import os, sys, getopt
 
 # Initialization
 config = ConfigParser()
-config.read(os.getcwd() + "/fb-test-runner.config")
-# Parse command lines
+config.read("./fb-test-runner.config")
+# Retrieve server path from config file or command line
+serverpath = config.get("update", "serverpath")
+# Parse command line
 try:
     opts, args = getopt.getopt(sys.argv[1:], "s:")
 except getopt.GetoptError, err:
-    print str(err) # will print something like "option -a not recognized"
+    print str(err)
     sys.exit(2)
-# Retrieve server path from config file or command line
-serverpath = config.get("update", "serverpath")
 for o, a in opts:
     if o == "-s":
         serverpath = a
@@ -27,8 +27,8 @@ test_bot = ConfigParser()
 test_bot.read(os.getcwd() + "/test-bot.config")
 # For each section in the config file, download the specified files and move them to the webserver
 for section in test_bot.sections():
-    os.system("wget -N --directory-prefix=./" + section.lower() + " " + test_bot.get(section, "FIREBUG_XPI"))
-    os.system("wget -N --directory-prefix=./" + section.lower() + " " + test_bot.get(section, "FBTEST_XPI"))
+    os.system("wget -N --output-document=./" + section.lower() + "/firebug.xpi" + " " + test_bot.get(section, "FIREBUG_XPI"))
+    os.system("wget -N --output-document=./" + section.lower() + "/fbtest.xpi" + " " + test_bot.get(section, "FBTEST_XPI"))
     if not os.path.isdir(os.path.curdir + section + "/.svn"):
         os.system("svn co " + " http://fbug.googlecode.com/svn/tests/" + " " + os.getcwd() + "/" + section.lower() + "/tests -r " + test_bot.get(section, "SVN_REVISION"))
     else:
