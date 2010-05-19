a10fa580c53cd272d8fc66bb132919754c791671
ahal <halbersa@gmail.com>
2010-05-19 11:17:36 -0700

    Send log file to stdout; automatically find default profile; additional error checking

--- fb-run.py
+++ fb-run.py
@@ -2,24 +2,27 @@ from ConfigParser import ConfigParser
 from time import sleep
 import os, sys, signal, subprocess, shlex, getopt, mozrunner
 
+def ping_firefox():
+    "See if Firefox is still running"
+    for line in os.popen("ps xa"):
+        fields = line.split()
+        pid = fields[0]
+        process = fields[4]
+        if process.find("firefox-bin") > 0 and process.find("<defunct>") == -1:
+            return pid
+    return 0
 
 def kill_firefox():
-    for line in os.popen("ps xa"):
-        fields = line.split()
-        pid = fields[0]
-        process = fields[4]
-        if process.find("firefox-bin") > 0 and process.find("<defunct>") == -1:
-            os.kill(int(pid), signal.SIGHUP)
-            break
-        
-def ping_firefox():
-    for line in os.popen("ps xa"):
-        fields = line.split()
-        pid = fields[0]
-        process = fields[4]
-        if process.find("firefox-bin") > 0 and process.find("<defunct>") == -1:
-            return 1
-    return 0
+    "Kill Firefox if it is running"
+    pid = ping_firefox()
+    if pid > 0:
+        os.kill(int(pid), signal.SIGHUP)
+
+def cleanup(exit_code):
+    "Perform cleanup and exit"
+    os.system("rm ./firebug.xpi")
+    os.system("rm ./fbtest.xpi")
+    sys.exit(exit_code)
 
 # Initialization
 config = ConfigParser()
@@ -51,25 +54,45 @@ else:
 # Grab the extensions
 os.system("wget -N " + serverpath + "/firebug.xpi " + serverpath + "/fbtest.xpi")
 
+if profile.lower() == "auto":
+    # Attempt to find the default profile automatically
+    p = subprocess.Popen("locate -r \\\\.mozilla/firefox.*\\\\.default$", shell=True, stdout=subprocess.PIPE)
+    try:
+        profile = p.communicate()[0]
+    except IndexError:
+        pass
+    
+print profile
+profile = str(profile[:-1])
+#print profile
+if not os.path.exists(profile):#"/home/mozilla/.mozilla/firefox/ty98qut7.default"):
+    #print "[Error] Profile doesn't exist: " + profile
+    cleanup(1)
+
+# Move existing log files to log_old folder (hidden)
+os.system("mv " + profile + "/firebug/fbtest/logs/* " + profile + "/firebug/fbtest/logs_old/")
+
 # If firefox is running, kill it (needed for mozrunner)
 kill_firefox()
-
 sleep(2)
-
+# Install the two extensions using mozrunner
 subprocess.Popen(shlex.split("mozrunner -n -p " + profile + " --addons=\"./firebug.xpi\",\"./fbtest.xpi\""))
 sleep(5)
+# Run Firefox with -runFBTests command line option
 subprocess.Popen("firefox -runFBTests " + serverpath + "/tests/content/testlists/firebug" + firebug_version + ".html", shell=True)
+sleep(2)
 
-count = 0
+# Find the 
+for name in os.listdir(profile + "/firebug/fbtest/logs"):
+    file = open(profile + "/firebug/fbtest/logs/" + name)
+
+# Send the log file to stdout as it arrives
 while ping_firefox():
-    sleep(1)
-    count += 1
-    if count % 30 == 0:
-        print "I'm not dead yet!"
+    line = file.readline()
+    if (line != ""):
+        print line[:-1]
 
 
 # Cleanup
-os.system("rm ./firebug.xpi")
-os.system("rm ./fbtest.xpi")
-sys.exit(0)
+cleanup(0)
 
