19e6e92f8e28375a9a7d79344f41efc1d27cf273
ahal <halbersa@gmail.com>
2010-06-07 16:05:09 -0700

    Add execute_fb_tests

--- execute_fb_tests.py
+++ execute_fb_tests.py
@@ -1,4 +1,4 @@
-import fb_run, ConfigParser, os, subprocess, sys
+import fb_run, ConfigParser, os, subprocess, sys, optparse, urllib2
 
 def retrieve_url(url, filename):
     ret = urllib2.urlopen(url)
@@ -8,11 +8,13 @@ def retrieve_url(url, filename):
 
 def main(argv):
     usage = "%prog [options]"
+    parser = optparse.OptionParser(usage)
     parser.add_option("-v", "--version", dest="version", default="1.6", help="The firebug version to run")
+    parser.add_option("-s", "--serverpath", dest="serverpath", default="http://localhost/", help="Server containing test-bot.config")
     (opt, remainder) = parser.parse_args(argv)
     
     lookup = { '3.5' : '1.9.1', '3.6' : '1.9.2', '3.7' : '1.9.3' }
-    retrieve_url(serverpath, "test-bot.config")
+    retrieve_url(opt.serverpath, "test-bot.config")
     
     config = ConfigParser.ConfigParser()
     config.read("test-bot.config")
