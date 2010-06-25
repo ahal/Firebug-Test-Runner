26399e5e8075e28bbee4081eddad5b1eb308736f
ahal <halbersa@gmail.com>
2010-06-25 10:31:12 -0700

    fb_log now sends progress information for failed tests to the database

--- fb_logs.py
+++ fb_logs.py
@@ -39,46 +39,55 @@ def main(argv):
         exit(1)
 
     reFirebugMeta = re.compile(r'^FIREBUG INFO \| ([^|:]*): (.*)') # 1: metadata item, 2: value
-
+    
+    reFirebugProgress = re.compile(r'^FIREBUG INFO \| ([^ ]*) \| ((\[OK\]|\[ERROR\]) .*)') # 1: file, 2: progress info
     reFirebugStart = re.compile(r'^FIREBUG INFO \| ([^ ]*) \| \[START\] (.*)') # 1: file, 2: description
     reFirebugResult = re.compile(r'^FIREBUG (TEST-[^ ]*) \| .*DONE') # 1: result
 
     checkForMeta = True
     testheaderdoc = {"type" : "header"}
     testresultdoc = None
+    testprogressinfo = ""
 
     logfilehandle = open(options.logfilename, 'r')
     for logline in logfilehandle:
         logline = logline.rstrip('\n')
 
-        #print logline
-
-        if checkForMeta:
-            match = reFirebugMeta.match(logline)
-            if match:
-                #print 'test: meta: %s=%s' % (match.group(1), match.group(2))
-                testheaderdoc[match.group(1)] = match.group(2)
-                testheaderdoc["App Changeset"] = options.changeset
-                testheaderdoc["CPU Architecture"] = platform["cpu"]
-                testheaderdoc["OS Detailed Name"] = platform["version"]
-
-        match = reFirebugStart.match(logline)
+        match = reFirebugProgress.match(logline)
         if match:
-            if checkForMeta:
-                headerinfo = couchdb.create(testheaderdoc)
-            checkForMeta = False
-            #print 'test: testfile=%s, testdescription=%s' % (match.group(1), match.group(2))
-            testresultdoc = dict(testheaderdoc)
-            testresultdoc["headerid"] = headerinfo["id"]
-            testresultdoc["type"] = "result"
-            testresultdoc["file"] = match.group(1)
-            testresultdoc["description"] = match.group(2)
+            testprogressinfo += match.group(2) + "\n"
         else:
-            match = reFirebugResult.match(logline)
+            if checkForMeta:
+                match = reFirebugMeta.match(logline)
+                if match:
+                    #print 'test: meta: %s=%s' % (match.group(1), match.group(2))
+                    testheaderdoc[match.group(1)] = match.group(2)
+                    testheaderdoc["App Changeset"] = options.changeset
+                    testheaderdoc["CPU Architecture"] = platform["cpu"]
+                    testheaderdoc["OS Detailed Name"] = platform["version"]
+
+            match = reFirebugStart.match(logline)
             if match:
-                #print 'test: result=%s' % match.group(1)
-                testresultdoc["result"] = match.group(1)
-                couchdb.create(testresultdoc)
+                if checkForMeta:
+                    headerinfo = couchdb.create(testheaderdoc)
+                checkForMeta = False
+                #print 'test: testfile=%s, testdescription=%s' % (match.group(1), match.group(2))
+                testresultdoc = dict(testheaderdoc)
+                testresultdoc["headerid"] = headerinfo["id"]
+                testresultdoc["type"] = "result"
+                testresultdoc["file"] = match.group(1)
+                testresultdoc["description"] = match.group(2)
+            else:
+                match = reFirebugResult.match(logline)
+                if match:
+                    #print 'test: result=%s' % match.group(1)
+                    testresultdoc["result"] = match.group(1)
+                    if testprogressinfo != "":
+                        testresultdoc["progress"] = testprogressinfo
+                        testprogressinfo = ""
+                    couchdb.create(testresultdoc)
+                    if "progress" in testresultdoc:
+                        del(testresultdoc["progress"])
 
     logfilehandle.close()
     return 0
