2507e105da5eddeb6ab737e3bbf23ee3fb86e800
ahal <halbersa@gmail.com>
2010-07-12 16:44:10 -0700

    Add license boilerplate

--- execute_fb_tests_template.py
+++ execute_fb_tests_template.py
@@ -1,9 +1,47 @@
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is the Firebug Test Runner.
+#
+# The Initial Developer of the Original Code is
+# Andrew Halberstadt.
+# Portions created by the Initial Developer are Copyright (C) 2010
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+# Andrew Halberstadt - ahalberstadt@mozilla.com
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
 from time import sleep
 import fb_run, ConfigParser, os, sys, optparse, urllib2, get_latest, tarfile, shutil
 
 # Global changeset variable
 changeset = {}
 
+# Save the file located at 'url' into 'filename'
 def retrieve_url(url, filename):
     try:
         ret = urllib2.urlopen(url)
@@ -14,11 +52,14 @@ def retrieve_url(url, filename):
     output.close()
     return 0
 
+# Return the changeset of the build located at 'buildpath'
 def get_changeset(buildpath):
     app_ini = ConfigParser.ConfigParser()
     app_ini.read(os.path.join(buildpath, "application.ini"))
     return app_ini.get("App", "SourceStamp")
-    
+
+# Return False if the tests have never been run against the current changeset of 'build'
+# True otherwise
 def build_needed(build, buildpath):
     # Find new changeset
     new_changeset = get_changeset(buildpath)
