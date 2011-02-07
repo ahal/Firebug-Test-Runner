d9dfcc545b68baeda8387b704412307b0ed467a2
ahal <halbersa@gmail.com>
2011-02-07 13:28:35 -0800

    Initial commit for buildbot integration of Firebug tests

--- runFBTests/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import execute_fb_tests
-import fb_update
-
-def cli_run():
-    execute_fb_tests.cli(sys.argv[1:])
-
-def cli_update():
-    fb_update.main(sys.argv[1:])
--- runFBTests/fb_run.py
+++ runFBTests/fb_run.py
@@ -41,7 +41,6 @@ from optparse import OptionParser
 from ConfigParser import ConfigParser
 import os, sys
 import mozrunner
-import fb_logs
 import fb_utils as utils
 import platform
 
@@ -52,10 +51,20 @@ class FBRunner:
         self.profile = kwargs["profile"]
         self.serverpath = kwargs["serverpath"]
         self.version = kwargs["version"]
-        self.couchURI = kwargs["couchURI"]
-        self.databasename = kwargs["databasename"]
         self.testlist = kwargs["testlist"]
         self.platform = platform.system().lower()
+        
+        # Ensure serverpath has correct format
+        self.serverpath = self.serverpath.rstrip("/") + "/"
+        
+        # Read in config file
+        utils.download(self.serverpath + "releases/firebug/test-bot.config", "test-bot.config")
+        self.config = ConfigParser()
+        self.config.read("test-bot.config")
+        
+        # Make sure we have a testlist
+        if !self.testlist:
+            self.testlist = self.config.get("Firebug"+self.version, "TEST_LIST");
 
     def cleanup(self):
         """
@@ -77,12 +86,8 @@ class FBRunner:
         Downloads the firebug and fbtest extensions
         for the specified Firebug version
         """
-        utils.download(self.serverpath + "releases/firebug/test-bot.config", "test-bot.config")
-        config = ConfigParser()
-        config.read("test-bot.config")
-        FIREBUG_XPI = config.get("Firebug" + self.version, "FIREBUG_XPI")
-        FBTEST_XPI = config.get("Firebug" + self.version, "FBTEST_XPI")
-        os.remove("test-bot.config")
+        FIREBUG_XPI = self.config.get("Firebug" + self.version, "FIREBUG_XPI")
+        FBTEST_XPI = self.config.get("Firebug" + self.version, "FBTEST_XPI")
         utils.download(FIREBUG_XPI, "firebug.xpi")
         utils.download(FBTEST_XPI, "fbtest.xpi")
 
@@ -114,16 +119,6 @@ class FBRunner:
                 for name in os.listdir(os.path.join(self.profile, "firebug/fbtest/logs")):
                     os.rename(os.path.join(self.profile, "firebug/fbtest/logs", name), os.path.join(self.profile, "firebug/fbtest/logs_old", name))
 
-        # Ensure serverpath has correct format
-        self.serverpath += ("" if self.serverpath[-1] == "/" else "/")
-        
-        # Ensure we have a testlist set
-        if not self.testlist:
-            self.testlist = self.serverpath + "tests/content/testlists/firebug" + self.version + ".html"
-
-        # If extensions were left over from last time, delete them
-        self.cleanup()
-
         # Grab the extensions from server   
         try:
             self.get_extensions()
@@ -137,7 +132,7 @@ class FBRunner:
         dict["XPC_DEBUG_WARN"] = "warn"     # Suppresses certain alert warnings that may sometimes appear
 
         # If firefox is running, kill it (needed for mozrunner)
-        mozrunner.kill_process_by_name("firefox" + (".exe" if self.platform == "windows" else "-bin"))
+        #mozrunner.kill_process_by_name("firefox" + (".exe" if self.platform == "windows" else "-bin"))
 
         # Create profile for mozrunner and start the Firebug tests
         print "[Info] Starting FBTests"
@@ -158,8 +153,8 @@ class FBRunner:
 
         # Find the log file
         timeout, logfile = 0, 0
-        # Wait up to 5 minutes for the log file to be initialized
-        while not logfile and timeout < 300:
+        # Wait up to 1 minute for the log file to be initialized
+        while not logfile and timeout < 60:
             try:
                 for name in os.listdir(os.path.join(self.profile, "firebug/fbtest/logs")):
                     logfile = open(os.path.join(self.profile, "firebug/fbtest/logs/", name))
@@ -181,21 +176,10 @@ class FBRunner:
                     timeout += 1
                 else:
                     timeout = 0
-                    
-        # Give last two lines of file a chance to write and send log file to fb_logs  
-        mozrunner.sleep(2)
-        filename = logfile.name
-        logfile.close()
-        print "[Info] Sending log file to couchdb at '" + self.couchURI + "'"
-        try:
-            fb_logs.main(["--log", filename, "--database", self.databasename, "--couch", self.couchURI,
-                             "--changeset", utils.get_changeset((self.binary if self.platform == "darwin" else os.path.dirname(self.binary)))])
-        except Exception as e:
-            print "[Error] Log file not sent to couchdb at server: '" + self.couchURI + "' and database: '" + self.databasename + "': " + str(e)
             
         # Cleanup
-        mozrunner.kill_process_by_name("crashreporter" + (".exe" if self.platform == "windows" else ""))
-        mozrunner.kill_process_by_name("firefox" + (".exe" if self.platform == "windows" else "-bin"))
+        #mozrunner.kill_process_by_name("crashreporter" + (".exe" if self.platform == "windows" else ""))
+        #mozrunner.kill_process_by_name("firefox" + (".exe" if self.platform == "windows" else "-bin"))
         self.cleanup()
 
 
@@ -209,27 +193,18 @@ def cli(argv=sys.argv[1:]):
                       help="The profile to use when running Firefox")
                         
     parser.add_option("-s", "--serverpath", dest="serverpath", 
-                      default="https://getfirebug.com",
+                      default="https://getfirebug.com/",
                       help="The http server containing the firebug tests")
                         
     parser.add_option("-v", "--version", dest="version",
-                      default="1.6",
+                      default="1.7",
                       help="The firebug version to run")
-                        
-    parser.add_option("-c", "--couch", dest="couchURI",
-                      default="http://localhost:5984",
-                      help="URI to couchdb server for log information")
-                        
-    parser.add_option("-d", "--database", dest="databasename",
-                      default="firebug",
-                      help="Database name to keep log information")
                         
     parser.add_option("-t", "--testlist", dest="testlist",
                       help="Specify the name of the testlist to use, should usually use the default")
     (opt, remainder) = parser.parse_args(argv)
     
-    runner = FBRunner(binary=opt.binary, profile=opt.profile, serverpath=opt.serverpath, version=opt.version, 
-                                        couchURI=opt.couchURI, databasename=opt.databasename, testlist=opt.testlist)
+    runner = FBRunner(binary=opt.binary, profile=opt.profile, serverpath=opt.serverpath, version=opt.version, testlist=opt.testlist)
     runner.run()
     
 if __name__ == '__main__':
